f5318b173a1d56200eb8a77fee7820e21cd12ce2
Turgay Ceylan <90829101+turgay2317@users.noreply.github.com>
2024-06-23 13:48:57 +0300

    custom-styles-feature

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,31 +4,51 @@ import HighlighterX from 'react-native-highlighter-x';
 
 export default function App() {
 	const rules = [
-		{
-		  pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-		  style: {color: 'blue'},
-		  onPress: (word:string) => Linking.openURL(`mailto:${word}`),
+	{
+		pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+		style: {color: 'white', backgroundColor: 'blue', padding: 3},
+		onPress: (word : string) => Linking.openURL(`mailto:${word}`),
+	},
+	{
+		pattern: /^https?:\/\/[^\s$.?#].[^\s]*$/,
+		style: {color: 'white', backgroundColor:'green'},
+		onPress: (word : string) => Linking.openURL(word),
+	},
+	{
+		pattern: /@(\w+)/g,
+		style: {color: '#0f0fc0', fontWeight:'bold'},
+		onPress: (username : string) => {
+			console.log("Clicked username: ", username);
 		},
-		{
-		  pattern: /^[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/,
-		  style: {color: 'green'},
-		  onPress: (word:string) => Linking.openURL(word),
-		},
-		{
-		  pattern: /@(\w+)/g,
-		  style: {color:'pink'},
-		  onPress: (username:string) => {
-			console.log("Clicked username: ",username)
-		  }
-		},
-		// Add more custom rules as needed
-	  ];
+		onLongPress: (username : string) => {
+			console.log("Long pressed : ", username);
+		}
+	},
+	{
+		pattern: /\b(\d{1,3}(\.\d{1,3}){3})\b/g,
+		style: {color: 'red'},
+		onPress: (ip : string) => {
+			console.log("Clicked IP address: ", ip);
+		}
+	},
+	{
+		pattern: /\b(\+?\d{1,4}?[-.\s]?)?\(?\d{1,4}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}\b/g,
+		style: {color: 'purple'},
+		onPress: (phoneNumber : string) => {
+			console.log("Clicked phone number: ", phoneNumber);
+		}
+	},
+	// Add more custom rules as needed
+	];
 
     return (
 		<View>
-        	<HighlighterX 
-				rules={rules} 
-				text={"Hello my name is @Turgay and my email is turgay2317@gmail.com my linkedin account is https://www.linkedin.com/in/turgayceylan/"}
+			<HighlighterX 
+				text={"Hello, today is a very beautiful day! If you want to check out our new project, you can visit https://npmjs.com For questions, you can reach turgay2317@gmail.com. Also, you can follow the users @turgay and @ali to stay updated with the latest developments. In case of an emergency, please contact us at +90 555 444 3322. Our server's IP address has been updated to 192.168.1.1."}
+				rules={rules}
+				customTextStyle={{fontWeight:'500'}}
+				customContainerStyle={{backgroundColor: 'white', padding:10}}
+				nonMatchTextStyle={{color:'#666'}}
 				/>
 		</View>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -2,48 +2,62 @@ import React from 'react';
 import { Text } from 'react-native';
 
 interface Rule {
-  pattern: RegExp;
-  style?: object;
-  onPress?: (word: string) => void;
-}
-
-interface HighlighterXProps {
-  text: string;
-  rules: Rule[];
-}
-
-export const HighlighterX: React.FC<HighlighterXProps> = ({ text, rules }) => {
-
-  const renderTextWithRules = (text: string, rules: Rule[]) => text && 
-    text.split(/\r?\n/).map((line, index) => (
-      <Text key={index}>
-        {renderWordsWithRules(line, rules)}
-        <Text>{"\n"}</Text>
-      </Text>
-    ));
-
-  const renderWordsWithRules = (row: string, rules: Rule[]) => {
-    return row.split(/(\s+)/).map((word, index) => {
-      const { style, onPress } = getMatchingRule(word, rules) || {};
-      return (
-        <Text key={index} style={style} onPress={() => onPress && onPress(word)}>
-          {word}
-        </Text>
-      );
-    });
+	pattern: RegExp;
+	style?: object;
+	onPress?: (word: string) => void;
+	onLongPress?: (word: string) => void;
+  }
+  
+  interface HighlighterXProps {
+	text: string;
+	rules: Rule[];
+	customContainerStyle?: object;
+	customTextStyle?: object;
+	nonMatchTextStyle? : object;
+  }
+  
+  export const HighlighterX: React.FC<HighlighterXProps> = ({ text, rules, customContainerStyle, customTextStyle, nonMatchTextStyle }) => {
+  
+	const renderTextWithRules = (text: string, rules: Rule[]) => text && 
+	  text.split(/\r?\n/).map((line, index) => (
+		<Text key={index}>
+		  {renderWordsWithRules(line, rules)}
+		  <Text>{"\n"}</Text>
+		</Text>
+	  ));
+  
+	const renderWordsWithRules = (row: string, rules: Rule[]) => {
+	  return row.split(/(\s+)/).map((word, index) => {
+		  const { style, onPress, onLongPress} = getMatchingRule(word, rules) || {};
+		  const handlePress = onPress ? () => onPress(word) : undefined;
+		  const handleLongPress = onLongPress ? () => onLongPress(word) : undefined;
+  
+		  return (
+			  <Text 
+				  key={index} style={[style || nonMatchTextStyle, customTextStyle]} 
+				  onPress={handlePress} 
+				  onLongPress={handleLongPress} 
+				  >
+			  {word}
+			  </Text>
+		  );
+	  });
+	};
+  
+	const getMatchingRule = (word: string, rules: Rule[]): { 
+	  style?: object, 
+	  onPress?: (word: string) => void,
+	  onLongPress? : (word: string) => void,
+	  } | null => {
+	  for (const rule of rules) {
+		const { pattern, style, onPress, onLongPress } = rule;
+		if (word.match(pattern)) {
+		  return { style, onPress, onLongPress };
+		}
+	  }
+	  return null;
+	};
+  
+	return <Text style={customContainerStyle}>{renderTextWithRules(text, rules)}</Text>;
   };
-
-  const getMatchingRule = (word: string, rules: Rule[]): { style?: object, onPress?: (word: string) => void } | null => {
-    for (const rule of rules) {
-      const { pattern, style, onPress } = rule;
-      if (word.match(pattern)) {
-        return { style, onPress };
-      }
-    }
-    return null;
-  };
-
-  return <Text>{renderTextWithRules(text, rules)}</Text>;
-};
-
 export default HighlighterX;
